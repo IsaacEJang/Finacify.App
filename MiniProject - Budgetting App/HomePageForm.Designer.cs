b30bcb1d350afbf1ba855cdbf4840305c526c039
Isaac E. Jang <euntak.jang@snhu.edu>
2024-06-18 16:36:23 -0700

    Add project files.

---
+++ MiniProject - Budgetting App/HomePageForm.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace MiniProject___Budgetting_App
+{
+    partial class HomePageForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // HomePageForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.SeaShell;
+            ClientSize = new Size(800, 450);
+            Name = "HomePageForm";
+            Text = "Home Page";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ MiniProject - Budgetting App/HomePageForm.cs
@@ -0,0 +1,10 @@
+namespace MiniProject___Budgetting_App
+{
+    public partial class HomePageForm : Form
+    {
+        public HomePageForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
