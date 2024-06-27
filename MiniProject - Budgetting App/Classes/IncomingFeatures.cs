5835dbfc772963431f0b8a5b8c443e720bb3e4e7
Isaac E. Jang <euntak.jang@snhu.edu>
2024-06-27 15:12:32 -0700

    incoming features class

---
+++ MiniProject - Budgetting App/Classes/IncomingFeatures.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniProject___Budgetting_App.Classes
+{
+    internal class IncomingFeatures
+    {
+        // Lists Features that has yet to be implemented
+
+        /*
+        
+        1. Each user has their own account. Currently data is shared across all the app
+
+        2. Be able to link account pull transactions from credit cards
+            2.1 implement feature to sort all the transactions
+
+        3. Security features
+            3.1 Forgot password
+            3.2 Email verifications
+            3.3 Phone number verification
+
+        4. budget
+            4.1 let the user choose what time of budget (conservative, relaxed, etc)
+
+        5. budget tracker
+            5.1 when toggling between months, show the budget for that month
+         */
+
+    }
+}
